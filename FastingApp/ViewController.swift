b792b93373396cc255efced86152bfe48dc61de0
Parag <porag989@gmail.com>
2020-06-30 14:09:28 +0600

    Initial Commit

---
+++ FastingApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FastingApp
+//
+//  Created by Twinbit Test on 30/6/20.
+//  Copyright © 2020 Twinbit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
